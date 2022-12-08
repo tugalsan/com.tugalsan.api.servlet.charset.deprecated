985f9f1c836d7b9caab68b692351c3eda41cde2b
tugalsan karabacak <tugalsan@gmail.com>
2022-12-09 00:04:41 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.tugalsan.api.servlet.charset.deprecated {
+    requires javax.servlet.api;
+    requires com.tugalsan.api.charset;
+    requires com.tugalsan.api.executable;
+    requires com.tugalsan.api.log;
+    requires com.tugalsan.api.unsafe;
+    exports com.tugalsan.api.servlet.charset.deprecated.server;
+}
---
+++ target/classes/com/tugalsan/api/servlet/charset/deprecated/server/TS_SCharSetWebFilterUTF8.java
@@ -0,0 +1,66 @@
+package com.tugalsan.api.servlet.charset.deprecated.server;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import com.tugalsan.api.charset.client.*;
+import com.tugalsan.api.log.server.*;
+import com.tugalsan.api.unsafe.client.*;
+
+@WebFilter(
+        urlPatterns = {"/*"},
+        initParams = {
+            @WebInitParam(name = "requestEncoding", value = TGS_CharSetUTF8.UTF8)
+        }
+)
+public class TS_SCharSetWebFilterUTF8 implements Filter {
+
+    final private static TS_Log d = TS_Log.of(TS_SCharSetWebFilterUTF8.class);
+
+    @Override
+    public void init(FilterConfig config) {
+        TGS_UnSafe.execute(() -> {
+            encoding = config.getInitParameter("requestEncoding");
+            if (encoding == null) {
+                encoding = TGS_CharSetUTF8.UTF8;
+            }
+        });
+    }
+    private String encoding;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain next) {
+        TGS_UnSafe.execute(() -> {
+
+            //REQUEST.COMMON
+            if (null == request.getCharacterEncoding()) {
+                request.setCharacterEncoding(encoding);
+            }
+
+            //REQUEST.HTML
+            if (String.valueOf(response.getContentType()).startsWith("text/html")) {
+                response.setContentType("text/html; charset=" + TGS_CharSetUTF8.UTF8);
+            }
+
+            //RESPONSE
+            response.setCharacterEncoding(TGS_CharSetUTF8.UTF8);
+
+            //ESCALATE
+            next.doFilter(request, response);
+        }, e -> {
+            if (e.getClass().getName().equals("org.apache.catalina.connector.ClientAbortException")) {
+                if (request instanceof HttpServletRequest hsr) {
+                    d.cr("doFilter", "CLIENT GAVE UP", e.getMessage(), hsr.getRequestURL().toString() + "?" + hsr.getQueryString());
+                } else {
+                    d.cr("doFilter", "CLIENT GAVE UP", e.getMessage());
+                }
+                return;
+            }
+            TGS_UnSafe.execute(() -> next.doFilter(request, response));//ESCALATE WITHOUT DEF_CHARSET
+        });
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
