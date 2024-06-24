1aca9e844d7480209ab0e8878e16197e6936e457
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-24 16:12:07 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.tugalsan.api.servlet.charset.deprecated {
     requires javax.servlet.api;
     requires com.tugalsan.api.charset;
-    requires com.tugalsan.api.runnable;
+    requires com.tugalsan.api.callable;
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.file;
